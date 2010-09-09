2ec202b4eeda256d56d8a40a1151719bc6dd904b
Dennis Weaver <loom@loomiverse.net>
2010-09-09 20:19:12 +1000

    Fixed some paths
    I should have run "brew audit" first.  I've learned that lesson :)

--- Library/Formula/kermit.rb
+++ Library/Formula/kermit.rb
@@ -6,11 +6,9 @@ class Kermit <Formula
   md5 '5767ec5e6ff0857cbfe2d3ec1ee0e2bc'
   version '8.0.211'
 
-  # depends_on 'cmake'
-
   def install
     system "make macosx103"
-    system "mkdir -p #{prefix}/share/man/man1"
-    system "make", "prefix=#{prefix}", "manroot=#{prefix}/share", "install"
+    system "mkdir -p #{man1}"
+    system "make", "prefix=#{prefix}", "manroot=#{}", "install"
   end
 end
