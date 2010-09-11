4777429c34f7a6ca4f202dc2b31a95fc60e804ee
Loom <loom@loomiverse.net>
2010-09-11 12:48:20 +1000

    Fixed chmox recipe, removed empty md5

--- Library/Formula/chmox.rb
+++ Library/Formula/chmox.rb
@@ -3,7 +3,6 @@ require 'formula'
 class Chmox <Formula
   head 'cvs://:pserver:anonymous@chmox.cvs.sourceforge.net:/cvsroot/chmox:Sources'
   homepage 'http://chmox.sourceforge.net'
-  md5 ''
 
   def install
     system "xcodebuild"
