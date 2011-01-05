048a1e72f8a45bd8ab9d874f1687a46380e37928
Loom <loom@loomiverse.net>
2011-01-05 11:33:01 +1100

    Added formula for freeradius

---
+++ Library/Formula/freeradius.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Freeradius <Formula
+  url 'ftp://ftp.freeradius.org//pub/freeradius//freeradius-server-2.1.10.tar.bz2'
+  homepage 'http://www.freeradius.org/'
+  md5 '8ea2bd39460a06212decf2c14fdf3fb8'
+  sha1 '3651ac3d50ec8b01ae46f9df5a50e31379c1e522'
+
+  version '2.1.10'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+
+    system "make"
+    
+    ENV.j1 # Install fails on multi-core otherwise.
+    system "make install"
+  end
+end
