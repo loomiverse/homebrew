0fda05b0e6f8264a718ee05d9351528ba893978e
Loom <loom@loomiverse.net>
2012-02-13 15:25:32 +1100

    Fixed tor changes made druing merge

--- Library/Formula/tor.rb
+++ Library/Formula/tor.rb
@@ -1,6 +1,5 @@
 require 'formula'
 
-<<<<<<< HEAD
 class Tor < Formula
   url 'https://www.torproject.org/dist/tor-0.2.2.35.tar.gz'
   homepage 'https://www.torproject.org/'
@@ -8,20 +7,6 @@ class Tor < Formula
 
   depends_on 'libevent'
 
-=======
-class Tor <Formula
-  url 'https://www.torproject.org/dist/tor-0.2.1.28.tar.gz'
-  homepage 'https://www.torproject.org/'
-  md5 '11e8384414623b50fe50ba7da85893db'
-  sha1 '31c6995314a7a205a5d685bffbe966a766c84d7b'
-
-  depends_on 'libevent'
-
-#  def patches
-#    {:p0 => 'https://gist.github.com/raw/344132/d27d1cd3042d7c58120688d79ed25a2fc959a2de/config.guess-x86_64patch.diff' }
-#  end
-
->>>>>>> 515f8c07ea8fa244433c700106eb12398d95f84c
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
