90e889a40987514c63c264aa97ae9875865b538a
Loom <loom@loomiverse.net>
2012-03-07 13:33:30 +1100

    Generate and install qch documentation

--- Library/Formula/soprano.rb
+++ Library/Formula/soprano.rb
@@ -12,10 +12,41 @@ class Soprano < Formula
   depends_on 'raptor' => :optional
   depends_on 'redland' => :optional
 
+  def patches
+    # generate qch file for documentation
+    DATA
+  end
+
   def install
     ENV['CLUCENE_HOME'] = HOMEBREW_PREFIX
 
     system "cmake . #{std_cmake_parameters}"
+    system "make apidox"
     system "make install"
+    prefix.install Dir['docs']
   end
 end
+
+__END__
+diff --git a/Doxyfile.cmake b/Doxyfile.cmake
+index cb8e9ad..8ec05a0 100644
+--- a/Doxyfile.cmake
++++ b/Doxyfile.cmake
+@@ -1,6 +1,16 @@
+ # Doxyfile 1.5.3
+ 
+ #---------------------------------------------------------------------------
++# qch related configuration options
++#---------------------------------------------------------------------------
++GENERATE_QHP            = yes
++QHP_NAMESPACE           = "org.kde.soprano"
++QHP_VIRTUAL_FOLDER      = "soprano"
++
++QCH_FILE                = "../soprano.qch"
++QHG_LOCATION            = "qhelpgenerator"
++
++#---------------------------------------------------------------------------
+ # Project related configuration options
+ #---------------------------------------------------------------------------
+ DOXYFILE_ENCODING      = UTF-8
+
